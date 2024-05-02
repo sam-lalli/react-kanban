7c61d2539eef403e834c5e1faca366d9d0d54e5f
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2024-05-02 17:51:54 -0600

    project setuo

---
+++ react-kanban/src/pages/Boards/index.tsx
@@ -0,0 +1,7 @@
+const Boards = () => {
+  return (
+    <div>Boards</div>
+  )
+}
+
+export default Boards
---
+++ react-kanban/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./indext.html", "./src/**/*.{js, ts, jsx, tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
